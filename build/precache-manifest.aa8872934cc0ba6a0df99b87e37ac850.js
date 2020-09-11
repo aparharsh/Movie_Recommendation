d336ce9c589e2ce7ad4efd6bf43579c35403f5ee
saurabh0373 <61005802+saurabh0373@users.noreply.github.com>
2020-09-12 00:42:08 +0530

    Add files via upload
    added updated build folder

---
+++ build/precache-manifest.aa8872934cc0ba6a0df99b87e37ac850.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32c1459336fe0096c566ecc1692c3485",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17d5a38c4df987184580",
+    "url": "/static/css/2.05a0ae32.chunk.css"
+  },
+  {
+    "revision": "159056b0f29d48554772",
+    "url": "/static/css/main.79c91cc8.chunk.css"
+  },
+  {
+    "revision": "17d5a38c4df987184580",
+    "url": "/static/js/2.076d336e.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.076d336e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "159056b0f29d48554772",
+    "url": "/static/js/main.84770207.chunk.js"
+  },
+  {
+    "revision": "419d296c21600b90fe58",
+    "url": "/static/js/runtime-main.dfac0e00.js"
+  },
+  {
+    "revision": "ce6cd9905fd21b2de9a570344d4e49d0",
+    "url": "/static/media/Mask Group 9.ce6cd990.png"
+  }
+]);
