d063dcc3008250a9403de88308d657fbf2d8f51e
aparharsh <38224372+aparharsh@users.noreply.github.com>
2020-09-12 22:19:24 +0530

    Add files via upload
    adding build with code changes in fetch url (without host address)

---
+++ build/precache-manifest.07809f9ff57fcd199379e27b69ab7651.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108f7a6e7ea2448eb9c498a04adecd91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b38472e7ccfc097ad471",
+    "url": "/static/css/2.05a0ae32.chunk.css"
+  },
+  {
+    "revision": "59b206ae43712eb4d805",
+    "url": "/static/css/main.79c91cc8.chunk.css"
+  },
+  {
+    "revision": "b38472e7ccfc097ad471",
+    "url": "/static/js/2.9a1a14cd.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.9a1a14cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59b206ae43712eb4d805",
+    "url": "/static/js/main.7da1f6cf.chunk.js"
+  },
+  {
+    "revision": "419d296c21600b90fe58",
+    "url": "/static/js/runtime-main.dfac0e00.js"
+  },
+  {
+    "revision": "ce6cd9905fd21b2de9a570344d4e49d0",
+    "url": "/static/media/Mask Group 9.ce6cd990.png"
+  }
+]);
